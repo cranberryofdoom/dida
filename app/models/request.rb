823ac59582c48985cfc0b780fde1b218bbc97b9b
Ashley Qian <ashley.qian@duke.edu>
2014-01-21 23:22:00 -0500

    file upload message taken care of

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,6 +1,4 @@
 class Request < ParseResource::Base
   fields :firstName, :lastName, :orgName, :cellNumber, :dueDate, :eMail, :fundCode, :orgDescription, :printMedium, :projectDetails, :projectDirection, :projectFiles, :requestType
-
-
   validates_presence_of :firstName, :lastName, :cellNumber, :dueDate, :eMail, :orgDescription, :projectDetails, :projectDirection, :requestType
 end
